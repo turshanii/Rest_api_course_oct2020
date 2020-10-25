2f0811850df5649f5776274056e66177d6c70784
turshanii <mourad.torsha@gmail.com>
2020-10-25 23:47:48 +0200

    fixed not running the app #6

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ api.add_resource(StoreList, '/stores')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000,debug=True)  # important to mention debug=True
+    app.run(debug=True)  # important to mention debug=True
